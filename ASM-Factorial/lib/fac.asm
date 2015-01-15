4e47b5e9e0b65e90f6e693ff22420e51dcd737c5
juhanr <juhan@risti.eu>
2015-01-15 13:34:41 +0200

    Refactored code: removed a few unnecessary lines, commented most lines.

--- ASM-Factorial/lib/fac.asm
+++ ASM-Factorial/lib/fac.asm
@@ -1,70 +1,61 @@
-SYS_EXIT  equ 1
-
 section .data
-
 	enterMessage db "Type a number to take factorial (-1 to exit): ", 0
-	readInput db "%d", 0
+	inputFormat db "%d", 0
 	resultMessage db "%d! = %d", 10, 0
-	badInputMsg db "The given number must be between 0 and 12!", 0xA	; factorials can't be taken from number 13 and higher because of the size limit of a integer
+	badInputMsg db "The given number must be between 0 and 12!", 10, 0
 
 section .bss
 	input resd 1
 
 section .text
-
 	global main
 	extern printf
 	extern scanf
 
 main:
 	; ask for input
-	push enterMessage
+	push enterMessage ; push the 'enter input' message to the stack
 	call printf
+	add esp, 4	; balance stack pointer
 
 	; read input
-	add esp,4
-	push input
-	push readInput
+	push input	; 'input' will hold the input value
+	push inputFormat ; 'inputFormat' gives the input format (an integer in a string)
 	call scanf
+	add esp, 8	; balance stack pointer
 
-	add esp, 8
-	mov ebx,[input]
+	mov ebx, [input] ; write input value into ebx
 
 	; check input
-	cmp ebx, -1
-	je exit
-	jl badInput
-	cmp ebx, 12
-	jg badInput
+	cmp ebx, -1 ; compare input with -1
+	je exit		; if input == -1, then jump to 'exit' label
+	jl badInput ; if input < -1, then jump to 'badInput' label
+	cmp ebx, 12 ; compare input with 12
+	jg badInput ; if input > 12, then jump to 'badInput' label
 
-	mov eax, 1
+	mov eax, 1	; eax will hold the factorial result, smallest possible result is 1
 
-facLoop:
-	cmp ebx, 1
-	jle writeResult
-	mul ebx
-	sub ebx,1
-	jmp facLoop
+facLoop:		; loop for calculating the factorial
+	cmp ebx, 1	; compare ebx with 1, as the value of ebx decreases by 1 with every loop and must get to 1 eventually
+	jle writeResult ; if ebx <= 1, then jump to label writeResult, because the calculation is complete
+	mul ebx		; eax = eax * ebx
+	sub ebx, 1	; decrement ebx by 1
+	jmp facLoop	; jump back to the beginning of the loop
 
 writeResult:
-	; display result
-	add esp,8
-	push eax
-	mov ebx, [input]
-	push ebx
-	push resultMessage
+	push eax	; push the result integer of the result message to the stack
+	mov ebx, [input] ; write the initial input to ebx
+	push ebx	; push the input integer of the result message to the stack
+	push resultMessage ; push the result message to the stack
 	call printf
-	add esp,12
-	jmp main
+	add esp, 12	; balance stack pointer
+	jmp main	; jump back to label 'main' to continue the program as a loop
 
-exit:    
-	mov eax, SYS_EXIT   
-	xor ebx, ebx 
-	int 0x80
+exit:			; exits the program
+	ret
 
-badInput:
-	add esp,8
-	push badInputMsg
+badInput:		; factorials can't be taken from number 13 and higher because of the size limit of a integer
+	push badInputMsg ; push the bad input message to the stack
 	call printf
-	add esp,4
-	jmp main
+	add esp, 4	; balance stack pointer
+	jmp main	; jump back to label 'main' to continue the program as a loop
